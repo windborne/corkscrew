ea1d9b261224d576d7b533183eaf5aeb4402ae80
Kai Marshland <kaimarshland@gmail.com>
2022-06-30 15:12:45 -0700

    Remove obsolete test file

--- test/run_test.rb
+++
@@ -1,7 +0,0 @@
-require_relative '../corkscrew/corkscrew'
-
-def deploy_from(config_path)
-  Corkscrew::Deployer.new(config_path, { local: config_path.include?('local') }).deploy
-end
-
-deploy_from ARGV[0]
